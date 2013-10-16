8a2478cf4051d7712107d3a81dadab4aba205f03
brunops <brunopsanches@gmail.com>
2013-10-16 15:40:53 -0700

    Quiz list being rendered in the homepage

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,13 +1,3 @@
-var Quizzes = Backbone.Collection.extend({
-  url: '/quizzes.json'
-})
-var quizzes = new Quizzes
-quizzes.fetch({
-  success: function(collection, response, options){
-    console.log(response)
-  }
-})
-
 var Quiz = Backbone.Model.extend({
   defaults: {
     id: '',
@@ -15,14 +5,62 @@ var Quiz = Backbone.Model.extend({
   }
 });
 
+var QuizView = Backbone.View.extend({
+  tagName: 'li',
+
+  render: function() {
+    var self = this;
+    this.$el.html(_.template($('#quiz-template').html(), { quiz: self.model }));
+    return this.el;
+  }
+});
+
+var QuizCollection = Backbone.Collection.extend({
+  url: '/quizzes.json',
+  model: Quiz,
+  parse: function(data) {
+    return data.quizzes;
+  }
+});
+
+var quizCollection = new QuizCollection
+
+var QuizzesView = Backbone.View.extend({
+  el: 'ul#quizzes',
+  initialize: function(options) {
+    this.collection = options.collection;
+  },
+  render: function() {
+    this.collection.each(function(quiz) {
+      var quizView = new QuizView({ model: quiz });
+      this.$el.append(quizView.render());
+    }, this);
+  }
+});
+
 var AppRouter = Backbone.Router.extend({
   routes: {
-    '': 'home'
+    '': 'home',
+    'quizzes/:id': 'startQuiz'
   }
 });
 var appRouter = new AppRouter;
 appRouter.on('route:home', function(){
-  console.log("HIT")
+  quizCollection.fetch({
+    success: function(collection, response, options){
+      new QuizzesView({collection: collection}).render();
+    }
+  });
 });
 
+appRouter.on('route:startQuiz', function(id) {
+  console.log(id)
+})
+
 Backbone.history.start();
+
+
+
+
+
+
